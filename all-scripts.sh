9c3a8837911df53d6dd62c8c7dc4afe3a1116140
Hips13 <vseivse@gmail.com>
2026-09-03 10:42:14 +0300

    Update all-scripts.sh

--- all-scripts.sh
+++ all-scripts.sh
@@ -2,16 +2,13 @@
 
 set -e
 
-echo "=== Настройка SSH ключей ==="
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/Hips13/info/refs/heads/main/scripts/setup-ssh.sh)"
-
 echo "=== Установка Cloudflare WARP ==="
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/Hips13/info/refs/heads/main/scripts/install-warp.sh)"
 
-echo "=== Обновление конфигурации Xray ==="
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/Hips13/info/refs/heads/main/scripts/change-xray-config.sh)"
-
 echo "=== Добавление обновления и рестарт в cron ==="
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/Hips13/info/refs/heads/main/scripts/update-restart.sh)"
+
+echo "=== Настройка SSH ключей ==="
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/Hips13/info/refs/heads/main/scripts/setup-ssh.sh)"
 
 echo "=== Все скрипты успешно выполнены! ==="
